c3db4a8310ef8c244f4bb612c1ac59d5a8b3bbd7
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2023-09-26 23:38:10 +0300

    Modify display

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,9 +6,16 @@ import * as jquery from "jquery"
 import "semantic-ui"
 import "channels"
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown()
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade')
   });
+  scroll_bottom();
 });
